8d2f480d89445c84973e4cdc6ffe749c25b8adf1
Jon Glick <jonathanpglick@gmail.com>
2011-10-24 17:47:10 -0700

    Region highlighting checkbox

--- js/ombuwire.js
+++ js/ombuwire.js
@@ -10,6 +10,8 @@ $(function() {
       $cloak = $('.cloak', $sidebar),
       $opener = $('.opener', $sidebar),
       $closer = $('.closer', $sidebar),
+      $regionsLabel = $('#ombuwire-regions-label'),
+      $regionsCheckbox = $('input', $regionsLabel),
       fullWidth;
 
   // move the sidebar far off screen so we can measure its full width
@@ -19,7 +21,6 @@ $(function() {
 
   // pull the sidebar just off the screen to the left
   $sidebar.css({left: '-'+fullWidth+'px'});
-
 
   // open/close click handlers
   function open(e) {
@@ -53,14 +54,69 @@ $(function() {
     $opener.hide();
   }
 
-
-  $sidebar.addClass('processed');
-
-
   $('a.noclick').click(function(e) {
     e.preventDefault();
     return false;
   });
+
+  function ucwords (str) {
+    return (str + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+        return $1.toUpperCase();
+    });
+  }
+
+  function regionsHighlight(e) {
+    if (e) { e.preventDefault(); }
+    $('body').addClass('ombuwire-region-outlines');
+    $.cookie('ombuwire.region-outlines', 1);
+
+    // Only run the first time
+    $('.region:not(.ombuwire-processed)').each(function() {
+      var $r = $(this);
+      if ($r.css('position') == 'static') {
+        $r.css('position', 'relative');
+      }
+
+      var classes = $r.attr("className").split(" "),
+          regionName;
+      for (var i = 0; i < classes.length; i++){
+        if (classes[i].substr(0, 7) == "region-"){
+          regionName = classes[i].substr(7).replace('-', ' ');
+          break;
+        }
+      }
+      $r.append('<label class="region-label">' + ucwords(regionName) + '</label>');
+      $r.addClass('ombuwire-processed');
+    });
+  }
+
+  function regionsUnhighlight(e) {
+    if (e) { e.preventDefault(); }
+    $('body').removeClass('ombuwire-region-outlines');
+    $.cookie('ombuwire.region-outlines', 0);
+  }
+
+  if ($('.region').length > 0) {
+    $regionsLabel.show();
+    $regionsCheckbox.change((function() {
+      return function(e) {
+        if ($(this).is(':checked')) {
+          regionsHighlight();
+        }
+        else {
+          regionsUnhighlight();
+        }
+      }
+    })());
+    if ($.cookie('ombuwire.region-outlines') == 1) {
+      $regionsCheckbox.attr('checked', 'checked');
+      regionsHighlight();
+    }
+  }
+
+
+  $sidebar.addClass('processed');
+
 });
 
 })(jQuery);
